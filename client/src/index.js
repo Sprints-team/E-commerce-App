c4746ee4e0afb1e15085f18f991a5755ec1119b3
DESKTOP-AA36RNR\Khaled <elkhorebykhaled@gmail.com>
2022-04-21 09:38:58 +0200

    Fix npm install

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,19 @@
-
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App tab='home' />
-    </BrowserRouter>
-  </Provider>
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 reportWebVitals();
